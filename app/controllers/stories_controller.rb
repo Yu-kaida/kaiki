ae50d16ff463550aad03f861b8a8dbd487290a5e
Yu-kaida <moondrip4869@gmail.com>
2023-09-19 00:47:04 +0900

    CRUD

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,22 @@ class StoriesController < ApplicationController
   def index
     @stories = Story.all.page(params[:page]).per(10)
   end
+
+  def new
+    @story = Story.new
+  end
+  
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to stories_path
+    else
+      render :new
+    end         
+  end
+
+  private
+    def story_params
+      params.require(:story).permit(:title, :content)
+    end
 end

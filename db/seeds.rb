2083131f2cc7992fdb2db762d020871f24368b09
Yu-kaida <moondrip4869@gmail.com>
2023-09-19 00:18:10 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# coding: utf-8
+Story.create(:title => 'Story 1', :content => 'Story 1 content')
+Story.create(:title => 'Story 2', :content => 'Story 2 content')
+Story.create(:title => 'Story 3', :content => 'Story 3 content')
+Story.create(:title => 'Story 4', :content => 'Story 4 content')
+Story.create(:title => 'Story 5', :content => 'Story 5 content')
+Story.create(:title => 'Story 6', :content => 'Story 6 content')
+Story.create(:title => 'Story 7', :content => 'Story 7 content')
+Story.create(:title => 'Story 8', :content => 'Story 8 content')
+Story.create(:title => 'Story 9', :content => 'Story 9 content')
+Story.create(:title => 'Story 10', :content => 'Story 10 content')
